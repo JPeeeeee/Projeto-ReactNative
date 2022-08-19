49b47035c77acf1453560cb7602661215aa9a94b
JPeeeeee <joaopedrodev8@gmail.com>
2022-08-19 14:12:12 -0300

    [FIX] Ajuste nos imports do Routes.jsx

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AuthRoutes from './auth.route';
-import AuthProvider from '../Contexts/auth';
 
 export default function Routes(){
     return (
         <NavigationContainer>
-            <AuthProvider>
-                <AuthRoutes/>
-            </AuthProvider>
+            <AuthRoutes/>
         </NavigationContainer>
     )
 }
